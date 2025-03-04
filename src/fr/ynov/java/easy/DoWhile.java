36a65a915f2ce344ee9e321038ddaa854dfa3fc8
bjoshua <joshua.budgen@ynov.com>
2025-03-04 17:28:13 +0100

    Exercise: Java
    - Loops: For, While, Do While
    - Array

---
+++ src/fr/ynov/java/easy/DoWhile.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            x++;
+            System.out.println(x);
+        } while (x < 10);
+    }
+}
