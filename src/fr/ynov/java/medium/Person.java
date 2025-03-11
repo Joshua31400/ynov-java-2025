8444df0c18727e6a0a96eae07f5040bc8356bd00
bjoshua <joshua.budgen@ynov.com>
2025-03-11 17:07:05 +0100

    Exercise: Java
    - Calculate age of Person

--- src/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/medium/Person.java
@@ -1,25 +1,33 @@
 package fr.ynov.java.medium;
 
+import java.time.LocalDate;
+
 public class Person {
     private String name;
-    private int age;
+    private String birthDate;
     private String gender;
     private float height;
     private float weight;
     private Nationality nationality;
 
-    public Person(String name, int age, String gender, float height, float weight, Nationality nationality) {
+    public Person(String name, String birthDate, String gender, float height, float weight, Nationality nationality) {
         this.name = name;
-        this.age = age;
+        this.birthDate = birthDate;
         this.gender = gender;
         this.height = height;
         this.weight = weight;
         this.nationality = nationality;
     }
 
+    public int getAge() {
+        LocalDate now = LocalDate.now();
+        LocalDate birth = LocalDate.parse(birthDate);
+        return now.getYear() - birth.getYear();
+    }
+
     public void printAttributes() {
         System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
+        System.out.println("Age: " + birthDate);
         System.out.println("Gender: " + gender);
         System.out.println("Height: " + height);
         System.out.println("Weight: " + weight);
